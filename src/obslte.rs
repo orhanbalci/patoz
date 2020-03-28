83c8aedba537975f4973f97c72a17c63f2bbeca5
orhanbalci@gmail.com <orhanbalci@gmail.com>
2020-03-28 23:02:22 +0300

    title record parser documentation added

--- src/obslte.rs
+++ src/obslte.rs
@@ -47,7 +47,27 @@ named!(
     )
 );
 
-named!( #[doc=r#""#],
+named!( #[doc=r#"
+Parses obslte records which is a continuation type of record. Continuation records are single records
+that span multi lines. There is only one OBSLTE record for each pdb file which is optional.
+If succesfull returns [Record](../ast/types/enum.Record.html) variant containing [Obslte](../ast/types/struct.Obslte.html) instance
+
+| COLUMNS    | DATA  TYPE    | FIELD         | DEFINITION                               |
+|------------|---------------|---------------|------------------------------------------|
+| 1 -  6     | Record name   | OBSLTE        |                                          |
+| 9 - 10     | Continuation  | continuation  | Allows concatenation of multiple records |
+| 12 - 20    | Date          | repDate       | Date that this entry was replaced.       |
+| 22 - 25    | IDcode        | idCode        | ID code of this entry.                   |
+| 32 - 35    | IDcode        | rIdCode       | ID code of entry that replaced this one. |
+| 37 - 40    | IDcode        | rIdCode       | ID code of entry that replaced this one. |
+| 42 - 45    | IDcode        | rIdCode       | ID code of entry  that replaced this one.|
+| 47 - 50    | IDcode        | rIdCode       | ID code of entry that replaced this one. |
+| 52 - 55    | IDcode        | rIdCode       | ID code of entry that replaced this one. |
+| 57 - 60    | IDcode        | rIdCode       | ID code of entry that replaced this one. |
+| 62 - 65    | IDcode        | rIdCode       | ID code of entry that replaced this one. |
+| 67 - 70    | IDcode        | rIdCode       | ID code of entry that replaced this one. |
+| 72 - 75    | IDcode        | rIdCode       | ID code of entry that replaced this one. |
+"#],
     pub obslte_record_parser<Record>,
     map!(obslte_line_folder, |obslte: Vec<u8>| {
         println!("{}", str::from_utf8(obslte.as_slice()).unwrap());
--- src/title.rs
+++ src/title.rs
@@ -1,3 +1,7 @@
+/*!
+Contains parsers related to [Title](http://www.wwpdb.org/documentation/file-format-content/format33/sect2.html#TITLE)
+records.The TITLE record contains a title for the experiment or analysis that is represented in the entry.
+*/
 use super::{ast::types::*, primitive::*};
 use nom::{
     character::complete::{line_ending, space0, space1},
@@ -30,8 +34,16 @@ named!(
 
 make_line_folder!(title_line_folder, title_line_parser, TitleLine);
 
-named!(
-    pub (crate) title_record_parser<Record>,
+named!( #[doc=r#"Parses title record which is a continuation record. This record may span multi lines. There is
+only one title record per pdb file. Record structure is given below
+
+| COLUMNS    | DATA  TYPE    | FIELD        | DEFINITION                                |
+|------------|---------------|--------------|-------------------------------------------|
+| 1 -  6     | Record name   | TITLE        |                                           |
+| 9 - 10     | Continuation  | continuation | Allows concatenation of multiple records. |
+| 11 - 80    | String        | title        | Title of the  experiment.                 |
+"#], 
+    pub title_record_parser<Record>,
     map!(
         title_line_folder,
         |title: Vec<u8>| if let Ok(res) = String::from_utf8(title) {
