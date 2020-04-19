b052d956285cea142f0cb6b073c3b11578b2fc0b
orhanbalci@gmail.com <orhanbalci@gmail.com>
2020-04-19 21:17:22 +0300

    mdltyp parser documented

--- src/mdltyp.rs
+++ src/mdltyp.rs
@@ -1,3 +1,8 @@
+/*!
+Contains parsers related to [Mdltyp](http://www.wwpdb.org/documentation/file-format-content/format33/sect2.html#MDLTYP) records.
+
+The MDLTYP record contains additional annotation pertinent to the coordinates presented in the entry.
+*/
 use super::{ast::types::*, primitive::*};
 use nom::{
     character::complete::{line_ending, space0, space1},
@@ -42,7 +47,19 @@ named!(
 );
 
 named!(
-    pub (crate) mdltyp_record_parser<Record>,
+#[doc=r#"
+Parses MDLTYP record which is a continuation record which can span multi lines. Contains `;` separated list of annotations. If successfull returns [Record](../ast/types/enum.Record.html) variant that contains [Mdltyp](../ast/types/struct.Mdltyp.html)
+
+*Record Structure:*
+
+| COLUMNS   | DATA TYPE     | FIELD        | DEFINITION                                 |
+|-----------|---------------|--------------|--------------------------------------------|
+| 1 -  6    | Record name   | MDLTYP       |                                            |
+| 9 - 10    | Continuation  | continuation | Allows concatenation of multiple records.  |
+| 11 - 80   | SList         | comment      | Free Text providing  additional structural |
+|           |               |              | annotation.                                |
+"#],
+    pub mdltyp_record_parser<Record>,
     map!(mdltyp_line_folder, |mdltyp: Vec<u8>| {
         if let Ok((_, res)) = mdltyp_parser(mdltyp.as_slice()) {
             res
